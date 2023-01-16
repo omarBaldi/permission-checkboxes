4f571d28ee3b85406695b66ce8a9062510279292
Omar Baldi <omar.baldi@maersk.com>
2023-01-16 13:35:15 +0100

    Define prop type definition for input checkbox custom component + handle logic for updating value through prop callback function

--- src/components/input-checkbox/dto.ts
+++ src/components/input-checkbox/dto.ts
@@ -1,3 +1,21 @@
-type InputCheckboxProps = {};
+import React from 'react';
+import { CheckboxLabel } from '../../App';
+
+type InputCheckboxProps = {
+  rowId: number;
+  checkboxKey: CheckboxLabel;
+  label: string;
+  checked: boolean;
+  additionalStyle?: React.CSSProperties;
+  onCheckboxChange: ({
+    rowId,
+    key,
+    updatedValue,
+  }: {
+    rowId: number;
+    key: string;
+    updatedValue: boolean;
+  }) => void;
+};
 
 export default InputCheckboxProps;
--- src/components/input-checkbox/input-checkbox.tsx
+++ src/components/input-checkbox/input-checkbox.tsx
@@ -1,8 +1,32 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import InputCheckboxProps from './dto';
 
-const InputCheckbox: FC<InputCheckboxProps> = ({}): JSX.Element => {
-  return <input type='checkbox' name='' id='' />;
+const InputCheckbox: FC<InputCheckboxProps> = ({
+  rowId,
+  checkboxKey,
+  label,
+  checked,
+  additionalStyle = {},
+  onCheckboxChange,
+}): JSX.Element => {
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked: updatedCheckedValue } = e.target;
+
+    const updatedParams: Parameters<typeof onCheckboxChange>[0] = {
+      rowId,
+      key: checkboxKey,
+      updatedValue: updatedCheckedValue,
+    };
+
+    onCheckboxChange({ ...updatedParams });
+  };
+
+  return (
+    <div style={{ ...additionalStyle }}>
+      <label>{label}</label>
+      <input type='checkbox' checked={checked} onChange={handleCheckboxChange} />
+    </div>
+  );
 };
 
 export default InputCheckbox;
